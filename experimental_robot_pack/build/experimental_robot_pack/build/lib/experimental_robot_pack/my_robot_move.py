2fc965aa176c6010a91a144cb510356da05a8035
NekrasovaAnn <114009080+NekrasovaAnn@users.noreply.github.com>
2023-12-22 14:01:43 +0700

    crossroad

--- experimental_robot_pack/build/experimental_robot_pack/build/lib/experimental_robot_pack/my_robot_move.py
+++ experimental_robot_pack/build/experimental_robot_pack/build/lib/experimental_robot_pack/my_robot_move.py
@@ -1,4 +1,5 @@
 from ast import Str
+from json import detect_encoding
 from std_msgs.msg import String
 import rclpy
 from rclpy.node import Node
@@ -8,7 +9,6 @@ from cv_bridge import CvBridge,CvBridgeError
 import cv2
 import numpy as np
 import time
-from rclpy.action import ActionClient
 import pkg_resources
 import os
 
@@ -20,6 +20,7 @@ class Camera(Node):
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
         self.pose_sub = self.create_subscription(Image, '/color/image', self.pose_callback, 1)
         self.p_ = self.create_publisher(String, '/robot_start', 1)
+        self.finish = self.create_publisher(String, '/robot_finish', 1)
         self.pose_m = self.create_subscription(String, '/robot_start',self.mesg,1)
         self.timer = self.create_timer(0.2, self.move_robot)
 
@@ -30,14 +31,17 @@ class Camera(Node):
 
         Camera.mstate = 0
         self.t = 0
+        self.timer = 0
+        self.right = 1  # 1 - right, 0 - left, -1 - nothing
 
         self.msg = np.zeros(1)
+        self.msg_depth = np.zeros(1)
         self.E = 0
         self.old_e = 0
         
-        self.Kp = 0.2#3.0
+        self.Kp = 0.25#3.0
         self.Ki = 0.001#0.1
-        self.Kd = 0.0#0.25
+        self.Kd = 0.001#0.25
         
 
     # Не двигаемся, пока нет разрешения
@@ -84,7 +88,7 @@ class Camera(Node):
 
 
     # Находит границы дороги на конкретной(line_num) горизонтальной линии изображения img
-    def find_borders(self, img, line_num=420): 
+    def find_borders(self, img, line_num=430): 
         lower_white = np.array([230, 230, 230])
         upper_white = np.array([255, 255, 255])
         white_mask = cv2.inRange(img, lower_white, upper_white)
@@ -128,7 +132,7 @@ class Camera(Node):
         img_center = (width + 1) // 2
         distance = -road_center + img_center - 28  # 28 - разница между центром изображения и центром робота
         cv2.circle(img, (road_center, 420), 5, (255, 0, 0), -1)
-        cv2.circle(img, (img_center, 420), 5, (0, 255, 0), -1)
+        cv2.circle(img, (img_center, 450), 5, (0, 255, 0), -1)
         cv2.imshow("k", img)
         cv2.waitKey(10)
         return -distance
@@ -156,15 +160,11 @@ class Camera(Node):
 
     # Обнаруживает дорожный знак на изображении
     # area - зона поиска(левая, правая, верхняя, нижняя границы), sign - изображение знака, image - изображение на котором искать, 
-    # size area - ожидаемые размеры знака, threshold - пороговый уровень уверенности
-    def detect_sign(self, sign, area = [0, 460, 200, 700], size_area = [100, 100], threshold = 0.6): 
+    # distances - порог соотношения расстояний Хэмминга, threshold - пороговый уровень уверенности по количеству расстояний Хэмминга
+    def detect_sign(self, sign, area=[50, 260, 250, 520], size_area=[95, 95], threshold=0.7):
         image = self.msg
         [x1, x2, y1, y2] = area
         img = image[x1:x2, y1:y2]
-
-        # cv2.imshow("k", img)
-        # cv2.waitKey(10)
-
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         sign = cv2.resize(sign, (size_area[0], size_area[1]), interpolation = cv2.INTER_AREA)  
         template = cv2.cvtColor(sign, cv2.COLOR_BGR2GRAY)
@@ -172,20 +172,9 @@ class Camera(Node):
         loc = np.where(res >= threshold)   
         for pt in zip(*loc[::-1]):  
             cv2.rectangle(img, pt,(pt[0] + size_area[0], pt[1] + size_area[1]),(0,255,255), 2)  
-            return True
-        return False # Возвращает true, если знак есть, false - если его нет
+            return np.argmax(res)
+        return 0 # Возвращает true, если знак есть, false - если его нет
 
-
-    # Проезжает перекресток
-    def crossroad(self, direction):
-        if direction == 1: # right
-            img = self.msg
-            img = img[0:len(img[0])-1, len(img[1])//2:len(img[1])-1]
-            return self.find_centre(img)
-        else: # left
-            img = self.msg
-            img = img[0:len(img[0])-1, 0:len(img[1])//2]
-            return self.find_centre(img)
 
 
     # Решает, когда пора двигать робота
@@ -193,7 +182,7 @@ class Camera(Node):
         message = Twist()
         k = 0
 
-        if self.t == 0: 
+        if self.t == 0: # определяет зеленый свет
             c = self.detect_green()
             if c : 
                 s = String()
@@ -205,35 +194,101 @@ class Camera(Node):
                     k = 1
                 else: k = 0
             else : self.t = 0
-        elif self.t == 1 : 
+        elif self.t == 1 : # ищет знак поворота
             detected_r = self.detect_sign(self.sign_r)
-            detected_l = self.detect_sign(self.sign_l)
-            if detected_r == False and detected_l == False : k = 1
-            elif detected_r : # Поворот направо
+            detected_l = self.detect_sign(self.sign_l, size_area=[112, 112])
+            if detected_l < 0.7 and detected_r < 0.7 : k = 1
+            elif detected_r > detected_l: # Поворот направо
                 print("right")  
                 self.t = 2
+                self.right = 1
+                print("право  ", detected_r)
+                print("лево   ", detected_l)
                 k=0
-            elif detected_l : # Поворот налево
+            elif detected_l > detected_r: # Поворот налево
                 print("left")  
                 self.t = 3
+                self.right = 0
+                print("право  ", detected_r)
+                print("лево   ", detected_l)
                 k=0
-        elif self.t == 2:
-            error = self.crossroad(1)
-            message.linear.x = 0.1
-            out = self.PID(error)
-            if error < 0 :
-                message.angular.z = float(out)
-            else : message.angular.z = float(-out)
+        elif self.t == 2:  # right
+            if self.timer>35: self.t = 4
+            elif self.timer > 20:
+                message.linear.x = 0.005
+                message.angular.z = -0.5
+            else:
+                message.linear.x = 0.1
+                message.angular.z = 0.0
             k=2
-        elif self.t == 3:
-            error = self.crossroad(2)
-            message.linear.x = 0.1
-            out = self.PID(error)
-            if error < 0 :
-                message.angular.z = float(out)
-            else : message.angular.z = float(-out)
+            self.timer += 1
+        elif self.t == 3:  # left
+            if self.timer > 37: self.t = 4
+            elif self.timer > 20:
+                message.linear.x = 0.005
+                message.angular.z = 0.5
+            else:
+                message.linear.x = 0.1
+                message.angular.z = 0.0
             k=2
-
+            self.timer += 1
+        elif self.t == 4:  # едет по перекрестку
+            k=1
+            self.t = 5
+            self.timer = 0
+        elif self.t == 5: 
+            if self.right == 1:
+                if self.timer < 82 : 
+                    k=1
+                else: 
+                    self.timer = 0
+                    self.t = 6
+            elif self.right == 0:
+                if self.timer < 68 : 
+                    k=1
+                else: 
+                    self.timer = 0
+                    self.t = 6
+            self.timer += 1
+            # k=0
+        elif self.t == 6 :  # выезжает с пеекрестка
+            k = 2
+            if self.right == 1: 
+                if self.timer < 20:
+                    message.linear.x = 0.065
+                    message.angular.z = -0.5
+                elif self.timer < 33:
+                    message.linear.x = 0.1
+                    message.angular.z = 0.0
+                else : 
+                    self.timer = 0
+                    self.t = 9
+            else :
+                if self.timer < 20:
+                    message.linear.x = 0.065
+                    message.angular.z = 0.5 
+                elif self.timer < 35:
+                    message.linear.x = 0.1
+                    message.angular.z = 0.0
+                else : 
+                    self.timer = 0
+                    self.t = 9
+            self.timer += 1
+        # elif self.t == 7:
+        #     if self.timer < 155 : 
+        #         self.right = 0
+        #         k=1
+        #     else :  
+        #         self.timer = 0
+        #         self.t = 8
+        #     self.timer += 1   
+        # elif self.t == 8:
+        #     self.t = 9 
+        elif self.t == 9 : # финиш
+            s = String()
+            s.data = "Name finished"
+            self.p_.publish(s)
+            k = 0
 
         if k == 1:
             error = self.find_centre(self.msg)
@@ -242,13 +297,11 @@ class Camera(Node):
                 message.angular.z = 0.0
             else:
                 message.linear.x = 0.1
-                out = self.PID(error)
-                # print(error)
-                # print(out)
-                if error < 0 :
-                    message.angular.z = float(out)
+                if self.right == 1: out = self.PID(error)
+                else : out = self.PID(-error)
+                if error < 0 : message.angular.z = float(out)
                 else : message.angular.z = float(-out)
-        elif k == 1: 
+        elif k == 0: 
             message.linear.x = 0.0
             message.angular.z = 0.0
 
